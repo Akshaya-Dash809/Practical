4bb8aa55412b1de06f306f7d39152526f0868b2d
Akshaya-Dash809 <111556367+Akshaya-Dash809@users.noreply.github.com>
2022-11-23 19:51:47 +0530

    Add files via upload

---
+++ CurrentThread.java
@@ -0,0 +1,14 @@
+package Lab_23_Nov;
+
+public class CurrentThread extends Thread{
+	public void run() {
+		System.out.println(Thread.currentThread().getName());
+	}
+	public static void main(String[] args) {
+		CurrentThread c=new CurrentThread();
+		CurrentThread c1=new CurrentThread();
+		c.start();
+		c1.start();
+	}
+
+}
---
+++ Threadeg2.java
@@ -0,0 +1,15 @@
+package Lab_23_Nov;
+
+//directly we use thread class without extends
+//with and without set name we can name the thread
+public class Threadeg2 {
+	
+	public static void main(String[] args) {
+		Thread t=new Thread("Pallabi");
+		Thread t1=new Thread();
+		t.start();
+		t1.start();
+		t1.setName("AKD");
+		System.out.println("Thread 0:"+t.getName());
+		System.out.println("Thread 1:"+t1.getName());
+}}
