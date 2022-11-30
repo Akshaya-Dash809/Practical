613b4132c6d334cb3d9069b8f2d730dc45014d0b
Akshaya-Dash809 <111556367+Akshaya-Dash809@users.noreply.github.com>
2022-11-30 19:01:27 +0530

    Add files via upload

---
+++ FileOutputStreamExample.java
@@ -0,0 +1,20 @@
+package Lab_30_Nov;
+
+
+import java.io.FileOutputStream;
+
+//string to byte value
+public class FileOutputStreamExample {
+	public static void main(String[] args) {
+		try {
+			FileOutputStream f = new FileOutputStream("D:\\test.txt"); // destination
+			f.write(67);
+			f.close();
+			System.out.println("Done");
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
