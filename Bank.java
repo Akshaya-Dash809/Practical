bd630e18ac260b891f09cb0fbab980d66d2a6b9d
Akshaya-Dash809 <111556367+Akshaya-Dash809@users.noreply.github.com>
2022-12-07 20:31:25 +0530

    Add files via upload

---
+++ Bank.java
@@ -0,0 +1,17 @@
+package Collection;
+
+
+public class Bank {
+	long accountNo;
+	String customerName;
+	double balance;
+	int customerAge;
+
+	public Bank(long acNo, String cName, double b, int ca) {
+		this.accountNo = acNo;
+		this.customerName = cName;
+		this.balance = b;
+		this.customerAge = ca;
+	}
+
+}
