438b5f9ea44c72a3edb45942b16ab51d4a2bc8f5
Akshaya-Dash809 <111556367+Akshaya-Dash809@users.noreply.github.com>
2022-12-14 09:17:13 +0530

    Add files via upload

---
+++ LinkedHashSetEg.java
@@ -0,0 +1,25 @@
+package Lab_13_Dec;
+
+
+import java.util.LinkedHashSet;
+
+public class LinkedHashSetEg {
+
+	public static void main(String[] args) {
+		// creating HashSet and adding elements
+		LinkedHashSet<String> lhs = new LinkedHashSet<String>();
+		lhs.add("Two");
+		lhs.add("One");
+		lhs.add("Three");
+		lhs.add("Four");
+		lhs.add("Five");
+		System.out.println("original list: " + lhs);
+		// removing specified element from the above mentioned list
+		System.out.println(lhs.remove("Two"));
+		System.out.println("updated list: " + lhs);
+		// six is not present
+		System.out.println(lhs.remove("Six")); // return false
+
+	}
+
+}
